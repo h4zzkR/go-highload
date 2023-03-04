a505028cd5bd1ce32f29a522743bd59e70c1fe37
h4zzkR <tramvenus@yandex.ru>
2023-03-04 19:10:38 +0300

    add web service

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+const SERVER = "http://localhost:8080"
+
+func main() {
+	data := url.Values{
+		"name": {"John Doe"},
+	}
+
+	resp, err := http.PostForm(SERVER+"/name", data)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	var res []map[string]string
+	json.NewDecoder(resp.Body).Decode(&res)
+	fmt.Println(res)
+}
---
+++ server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type DateResponseItem struct {
+	Year  uint `json:"year"`
+	Month uint `json:"month"`
+	Day   uint `json:"day"`
+}
+
+type NameResponseItem struct {
+	Name string `json:"name"`
+}
+
+type DateResponse []DateResponseItem
+type NameResponse []NameResponseItem
+
+const RESPONSE_ITEMS_SIZE = 10000
+
+func date(w http.ResponseWriter, req *http.Request) {
+	log.Println("json1")
+
+	nowDate := time.Now()
+
+	res := make([]DateResponseItem, RESPONSE_ITEMS_SIZE)
+	for i := 0; i < RESPONSE_ITEMS_SIZE; i++ {
+		res[i] = DateResponseItem{
+			Year:  uint(nowDate.Year()),
+			Month: uint(nowDate.Month()),
+			Day:   uint(nowDate.Day()),
+		}
+	}
+
+	resMarshalled, _ := json.Marshal(res)
+	fmt.Fprintf(w, string(resMarshalled))
+}
+
+func name(w http.ResponseWriter, req *http.Request) {
+	log.Println("name")
+
+	if req.Method == "POST" {
+		if err := req.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		name := req.PostForm.Get("name")
+
+		res := make([]NameResponseItem, RESPONSE_ITEMS_SIZE)
+		for i := 0; i < RESPONSE_ITEMS_SIZE; i++ {
+			res[i] = NameResponseItem{
+				Name: name,
+			}
+		}
+
+		resMarshalled, _ := json.Marshal(res)
+		fmt.Fprintf(w, string(resMarshalled))
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/date", date)
+	http.HandleFunc("/name", name)
+
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8080", nil)
+}
